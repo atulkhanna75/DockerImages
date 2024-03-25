9067dd13c0dee7be9a8e17710951335e52ff1f37
Atul Khanna <atulkhanna75@gmail.com>
2024-03-25 06:40:37 +0000

    done

---
+++ Java images/Demo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Demo{
+    public static void main (String[] args){
+        System.out.println("Simple math operation");
+    //     Scanner sc=new Scanner(System.in);
+    //    System.out.println("Enter value of a");
+    //    int a=sc.nextInt();
+    //    System.out.println("Enter value of b");
+    //     int b=sc.nextInt();
+    //     int sum=a+b;
+    //    System.out.println("Sum of 2 number: "+sum);
+    //     sc.close();
+    }
+}
---
+++ Springboot Image/spring-docker-rest/src/test/java/com/okta/mongodb/springdockerrest/SpringDockerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.mongodb.springdockerrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
